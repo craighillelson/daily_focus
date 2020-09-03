385e1ff1182a34605e64eea90807c170d84134a7
craighillelson <craighillelson@gmail.com>
2020-09-03 00:12:16 -0400

    update functions.py
    added get_target_date function

--- functions.py
+++ functions.py
@@ -1,11 +1,14 @@
-"""Functions"""
+"""Functions."""
 
 import csv
+import datetime
 from datetime import (date,
                       timedelta)
+import pyinputplus as pyip
 
 target_date = date(2021, 8, 7)
 todays_date = date.today()
+date_tasks_to_add = {}
 dates_tasks = {}
 
 
@@ -19,12 +22,39 @@ def diff_dates():
     return abs((target_date - todays_date).days)
 
 
+def get_target_date():
+    """Import "target_date.txt" and return the date contained in the file."""
+
+    date_file = open("target_date.txt", "r")
+    imported_target_date = date_file.read()
+    datetime_obj = datetime.datetime.strptime(imported_target_date, "%Y-%m-%d")
+    return datetime_obj.date()
+
+
 def greeting():
     """Greet user."""
     print("\nGood morning!")
 
 
-def tasks(user_prompt):
+def open_csv_pop_dct():
+    """Open csv and populate a dictionary with its contents."""
+
+    dct = {}
+
+    with open("dates_tasks.csv", newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            dct[row["date"]] = row["task_start_of_day"]
+
+    return dct
+
+
+def prompt_user_for_target_date(user_prompt):
+    """Prompt user for target date."""
+    return pyip.inputDate(user_prompt, formats=["%Y-%m-%d"])
+
+
+def prompt_user_for_task(user_prompt):
     """Prompt user for input."""
     return input(f"\n{user_prompt}")
 
