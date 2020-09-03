ea603b0c63151fdcd5539ba27a1948bddd70bd07
craighillelson <craighillelson@gmail.com>
2020-09-03 00:11:49 -0400

    update daily_focus.py
    added logic to handle a csv with no entries

--- daily_focus.py
+++ daily_focus.py
@@ -1,20 +1,27 @@
 """Script to track daily tasks in pursuit of a goal."""
 
 from functions import (target_date,
-                       days_out,
                        todays_date,
+                       date_tasks_to_add,
                        dates_tasks,
+                       open_csv_pop_dct,
+                       days_out,
                        diff_dates,
                        greeting,
-                       tasks,
+                       prompt_user_for_task,
                        write_dct_to_csv)
 
+dates_tasks_from_file = open_csv_pop_dct()
 greeting()
 print("\ntarget date:", target_date)
 print(f"90 days out: {days_out(90)}")
 print(f"180 days out: {days_out(180)}")
-to_do_today = tasks("What would you like to work on today?\n> ")
-dates_tasks[todays_date] = to_do_today
+to_do_today = prompt_user_for_task("What would you like to work on today?\n> ")
+if dates_tasks_from_file:
+    date_tasks_to_add[todays_date] = to_do_today
+    dates_tasks = {**dates_tasks_from_file, **date_tasks_to_add}
+else:
+    dates_tasks[todays_date] = to_do_today
 write_dct_to_csv("dates_tasks.csv", dates_tasks)
 days_from_target = diff_dates()
 print(f"days from target: {days_from_target}\n")
