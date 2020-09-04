ea9555338383250761c89769230e90e40ee324d9
craighillelson <craighillelson@gmail.com>
2020-09-04 07:57:55 -0400

    create target_date.py
    first commit

---
+++ target_date.py
@@ -0,0 +1,14 @@
+"""Prompt user for a target date."""
+
+from functions import (todays_date,
+                       prompt_user_for_target_date)
+
+while True:
+    target_date = prompt_user_for_target_date("\nEnter a target date in "
+                                              "YYYY-MM-DD format \n> ")
+    if target_date > todays_date:
+        FILE = open("target_date.txt", "w")
+        FILE.write(str(target_date))
+        break
+    else:
+        print("Please enter a date in the future.")
