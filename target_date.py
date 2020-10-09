8b1ed2cb1eca638f904ed1bde1a9634f2e74aefc
Craig Hillelson <craighillelson@gmail.com>
2020-10-09 10:19:55 -0400

    update target_date.py
    linted code

--- target_date.py
+++ target_date.py
@@ -5,10 +5,9 @@ from functions import (todays_date,
 
 while True:
     target_date = prompt_user_for_target_date("\nEnter a target date in "
-                                              "YYYY-MM-DD format \n> ")
+                                              "YYYY-MM-DD format\n> ")
     if target_date > todays_date:
         FILE = open("target_date.txt", "w")
         FILE.write(str(target_date))
-        break
     else:
         print("Please enter a date in the future.")
